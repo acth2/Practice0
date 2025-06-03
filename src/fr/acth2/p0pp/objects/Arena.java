3a00e2cc42d08bb58c4e6bed53313228b80ce4fd
acth2 <iliane57950@gmail.com>
2025-06-03 23:22:56 +0200

    Completing the Arena.java class

--- src/fr/acth2/p0pp/objects/Arena.java
+++ src/fr/acth2/p0pp/objects/Arena.java
@@ -1,5 +1,6 @@
 package fr.acth2.p0pp.objects;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
@@ -19,6 +20,56 @@ public class Arena {
         player2spawn = this.player2spawn;
         start = this.start;
     }
+
+    public void startDuel() {
+        if (player1 != null && player2 != null) {
+            player1.teleport(player1spawn);
+            player2.teleport(player2spawn);
+            sendMessages(null);
+        }
+     }
+
+     public void sendMessages(Player definedWinner) {
+        if (definedWinner == null) {
+            player1.sendMessage(ChatColor.GOLD + "===================");
+            player1.sendMessage(ChatColor.YELLOW + "Player " + player1.getName() + " (you)" + ChatColor.GRAY + " against");
+            player1.sendMessage(ChatColor.YELLOW + "Player " + player2.getName());
+            player1.sendMessage(ChatColor.GRAY +  "&oMay you be the winner of this fight");
+            player1.sendMessage(ChatColor.GOLD + "===================");
+
+            player2.sendMessage(ChatColor.GOLD + "===================");
+            player2.sendMessage(ChatColor.YELLOW + "Player " + player2.getName() + " (you)" + ChatColor.GRAY + " against");
+            player2.sendMessage(ChatColor.YELLOW + "Player " + player1.getName());
+            player2.sendMessage(ChatColor.GRAY +  "&oMay you be the winner of this fight");
+            player2.sendMessage(ChatColor.GOLD + "===================");
+        } else {
+            if (definedWinner == player1) {
+                player1.sendMessage(ChatColor.GOLD + "=================== RESULTS");
+                player1.sendMessage(ChatColor.GREEN + "Player " + player1.getName() + " won");
+                player1.sendMessage(ChatColor.RED + "Player " + player2.getName() + " lose");
+                player1.sendMessage(ChatColor.GRAY +  "&oYou won the fight!");
+                player1.sendMessage(ChatColor.GOLD + "=================== RESULTS");
+
+                player2.sendMessage(ChatColor.GOLD + "=================== RESULTS");
+                player2.sendMessage(ChatColor.GREEN + "Player " + player1.getName() + " won");
+                player2.sendMessage(ChatColor.RED + "Player " + player2.getName() + " lose");
+                player2.sendMessage(ChatColor.GRAY +  "&oYou lose the fight!");
+                player2.sendMessage(ChatColor.GOLD + "=================== RESULTS");
+            } else {
+                player2.sendMessage(ChatColor.GOLD + "=================== RESULTS");
+                player2.sendMessage(ChatColor.GREEN + "Player " + player2.getName() + " won");
+                player2.sendMessage(ChatColor.RED + "Player " + player1.getName() + " lose");
+                player2.sendMessage(ChatColor.GRAY +  "&oYou won the fight!");
+                player2.sendMessage(ChatColor.GOLD + "=================== RESULTS");
+
+                player1.sendMessage(ChatColor.GOLD + "=================== RESULTS");
+                player1.sendMessage(ChatColor.GREEN + "Player " + player2.getName() + " won");
+                player1.sendMessage(ChatColor.RED + "Player " + player1.getName() + " lose");
+                player1.sendMessage(ChatColor.GRAY +  "&oYou lose the fight!");
+                player1.sendMessage(ChatColor.GOLD + "=================== RESULTS");
+            }
+        }
+     }
 
     public boolean hasStarted() {
         return start;
